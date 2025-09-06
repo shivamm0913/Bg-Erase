2ff484134100e04bd9789f270f1922a87586903f
Shivam Kewat <shivamkewat0209@gmail.com>
2025-09-06 19:05:11 +0530

    backend setup3

--- server/server.js
+++ server/server.js
@@ -11,17 +11,15 @@ await connectDB(); // connect lazily, only when needed
 // Middleware to parse JSON requests
 app.use(express.json());
 app.use(cors());
+
 //API routes
 app.get("/", async (req, res) => {
   res.send("API Working 🚀");
 });
-// Start the server
-// app.listen(PORT, () => {
-//   console.log(`Server running on http://localhost:${PORT}`);
-// });
+
 if (process.env.NODE_ENV !== "production") {
   app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
   });
 }
-// export default app;
+export default app;
