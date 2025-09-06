1d7a057f2612b5ffc741259969193063f9407321
Shivam Kewat <shivamkewat0209@gmail.com>
2025-09-06 18:33:01 +0530

    vercel backend setup

--- server/server.js
+++ server/server.js
@@ -17,6 +17,7 @@ app.get("/", (req, res, next) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
+export default app;
